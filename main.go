f611f3ebb7b6691749b48ce1d79b9fc962ae093d
Higor Diego <higordiegoti@gmail.com>
2020-01-25 21:18:20 -0300

    aula do alura 01

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Higor Diego"
+	var numerAgencia int = 489
+	var numeroConta int = 123456
+	var saldo float64 = 125.20
+	fmt.Println(titular, numerAgencia, numeroConta, saldo)
+
+}
